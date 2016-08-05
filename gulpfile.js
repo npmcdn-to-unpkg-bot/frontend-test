e0957d967a9955c3467f7c79c96fbda7b0196b2a
Takashi Taniguchi <t.taniguchi.1979@gmail.com>
2016-08-05 11:31:45 +0900

    added react + redux

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
-var gulp = require('gulp');
-var pkg = require('./package.json');
+var gulp        = require('gulp');
+var pkg         = require('./package.json');
 
-var webserver = require('gulp-webserver');
+var react       = require('gulp-react');
+var plumber     = require('gulp-plumber');
+var webserver   = require('gulp-webserver');
 
+// Javascript
+gulp.task('js', function () {
+  gulp.src('./src/vendor/*.js')
+    .pipe(gulp.dest('./public/assets/js/'));
+  gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./public/assets/js/'));
+
+  return gulp.src('./src/jsx/*.jsx')
+    .pipe(plumber())
+    .pipe(react())
+    .pipe(gulp.dest('./public/assets/js/'));
+});
+
+// watch
+gulp.task('watch', function(){
+  gulp.watch('./src/jsx/*.jsx', ['js']);
+});
 // webserver reload
 gulp.task('webserver', function () {
-    gulp.src('public')
+    gulp.src('./public')
         .pipe(webserver({
             host: 'localhost',
             port: 3000,
@@ -13,4 +32,4 @@ gulp.task('webserver', function () {
         }));
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('default', ['js', 'watch', 'webserver']);
